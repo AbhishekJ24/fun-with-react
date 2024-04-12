dd6108263955ec62fa88f5643a814c30000efd0a
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-04-13 01:20:54 +0530

    reinit directory, can be used for unit testing

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,10 @@
 import "./globals.css";
-import Header from '@/Components/Header'
-import Footer from '@/Components/Footer';
-import Separator from "@/Components/Separator";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Header />
-        <Separator/>
         {children}
-        <Separator/>
-        <Footer />
       </body>
     </html>
   );
